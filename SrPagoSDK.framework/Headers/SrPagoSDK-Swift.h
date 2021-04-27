d10238aacd4470cd3c8537e74e50e624799e030a
Jorge Mejia <jmejiah@srpago.com>
2021-04-27 08:42:39 -0500

    Update with last changes

--- SrPagoSDK.framework/Headers/SrPagoSDK-Swift.h
+++ SrPagoSDK.framework/Headers/SrPagoSDK-Swift.h
@@ -253,9 +253,9 @@ SWIFT_CLASS_PROPERTY(@property (nonatomic, class) BOOL showLogs;)
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
 + (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
 - (void)makePayment:(UIViewController * _Nonnull)from payment:(Payment * _Nonnull)payment successful:(void (^ _Nonnull)(BOOL))successful;
-- (void)authWithCredentialsWithKey:(NSString * _Nonnull)key secret:(NSString * _Nonnull)secret;
-- (void)authUser:(NSString * _Nonnull)user password:(NSString * _Nonnull)password;
+- (void)config;
 @end
+
 
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
@@ -517,9 +517,9 @@ SWIFT_CLASS_PROPERTY(@property (nonatomic, class) BOOL showLogs;)
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
 + (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
 - (void)makePayment:(UIViewController * _Nonnull)from payment:(Payment * _Nonnull)payment successful:(void (^ _Nonnull)(BOOL))successful;
-- (void)authWithCredentialsWithKey:(NSString * _Nonnull)key secret:(NSString * _Nonnull)secret;
-- (void)authUser:(NSString * _Nonnull)user password:(NSString * _Nonnull)password;
+- (void)config;
 @end
+
 
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
