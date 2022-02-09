af36eb3a61087177bdc6a858f8eef8d280354cda
Fernando Bustos Ramirez <fbustosr@srpago.com>
2022-02-08 19:10:09 -0600

    [ADDED]: New xcframeworks

---
+++ KNCore.xcframework/ios-arm64/KNCore.framework/Headers/KNCore.h
@@ -0,0 +1,18 @@
+//
+//  KNCore.h
+//  KNCore
+//
+//  Created by Fernando Bustos on 26/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KNCore.
+FOUNDATION_EXPORT double KNCoreVersionNumber;
+
+//! Project version string for KNCore.
+FOUNDATION_EXPORT const unsigned char KNCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KNCore/PublicHeader.h>
+
+
